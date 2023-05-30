f089b0307b4dfd6892d7f7e03c360bf72826382e
Osailton <osailtonvieira@gmail.com>
2023-05-30 16:05:06 -0300

    Project created

---
+++ src/main/java/com/springmongo/SampleSpringMongoApplication.java
@@ -0,0 +1,13 @@
+package com.springmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringMongoApplication.class, args);
+	}
+
+}
